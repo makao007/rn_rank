993ba4f160352097770dd50562f7bf20f58c8944
makao007 <makao007@126.com>
2016-07-17 18:28:54 +0800

    fix load more and then refrash error

--- app/reducers/read.js
+++ app/reducers/read.js
@@ -17,24 +17,27 @@ for (var i=0; i<info.API_MENUS.length; i++) {
 export default function read(state = initialState, action) {
 	switch (action.type) {
 		case types.FETCH_ARTICLE_LIST:
-			console.log ('fetch list ', action.category, 'index',info.categoryIndex(action.category));
 			state[info.categoryIndex(action.category)].isRefreshing = action.isRefreshing;
 			return Object.assign({}, state);
 		break;
 		case types.RECEIVE_ARTICLE_LIST:
-			console.log('got article list, ',action.category, info.categoryIndex(action.category), action.rankList.data);
 			var tmp = info.categoryIndex(action.category);
 			state[tmp].isRefreshing = action.isRefreshing;
 			state[tmp].articleList = action.rankList.data;
 			state[tmp].isFirstLoaded = false;
+			state[tmp].index = 1;
+			state[tmp].noMore = false;
 			return Object.assign({}, state);
 		break;
 		case types.RECEIVE_ARTICLE_LIST_MORE:
-			console.log (action, action.nowRead);
 			var tmp = info.categoryIndex(action.category);
 			state[tmp].isRefreshing = action.isRefreshing;
 			state[tmp].articleList = state[tmp].articleList.concat(action.rankList.data);
-			state[tmp].index = state[tmp].index + 1;;
+			if (action.rankList.data.length==0) {
+				state[tmp].noMore = true;
+			} else {
+				state[tmp].index = state[tmp].index + 1;
+			}
 			return Object.assign({}, state);
 		break;
 		default:
